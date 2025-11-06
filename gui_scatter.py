c12fc0797d3fe27e1b69802289f36960dd0b0fd0
Nicolas <nicolas.goldbach@epfl.ch>
2025-11-06 22:03:58 +0100

    added range sliders

--- gui_scatter.py
+++ gui_scatter.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QGroupBox, QFormLayout, QPushButton, QComboBox, QSpinBox, QFileDialog
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QSlider, QGroupBox, QFormLayout, QPushButton, QComboBox, QSpinBox, QFileDialog
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 from matplotlib.widgets import LassoSelector
@@ -33,9 +33,30 @@ class ScatterTab:
         self.max_models_spin.setRange(1,200)
         self.max_models_spin.setValue(10)
 
-        #form.addRow("Load CSV:", self.load_btn)
+        # Min / Max sliders for Zoom
+        self.x_min_slider = QSlider()
+        self.x_max_slider = QSlider()
+        self.y_min_slider = QSlider()
+        self.y_max_slider = QSlider()
+        for s in [self.x_min_slider, self.x_max_slider, self.y_min_slider, self.y_max_slider]:
+            s.setOrientation(1)  # Horizontal
+            s.setMinimum(0)
+            s.setMaximum(100)
+            s.valueChanged.connect(self.plot_scores)
+        
+        # set defaults
+        self.x_min_slider.setValue(0)
+        self.y_min_slider.setValue(0)
+        self.x_max_slider.setValue(100)
+        self.y_max_slider.setValue(100)
+
+        # construct layout
         form.addRow("X-axis:", self.x_combo)
+        form.addRow("X Min:", self.x_min_slider)
+        form.addRow("X Max:", self.x_max_slider)
         form.addRow("Y-axis:", self.y_combo)
+        form.addRow("Y Min:", self.y_min_slider)
+        form.addRow("Y Max:", self.y_max_slider)
         form.addRow("Plot:", self.plot_btn)
         form.addRow("Max models:", self.max_models_spin)
         # TODO: classification
@@ -57,6 +78,19 @@ class ScatterTab:
         if self.plugin.df is None: return
         x = self.x_combo.currentText()
         y = self.y_combo.currentText()
+        if x=="" or y=="": return
+
+        x_data = self.plugin.df[x]
+        y_data = self.plugin.df[y]
+        x_min, x_max = np.min(x_data), np.max(x_data)
+        y_min, y_max = np.min(y_data), np.max(y_data)
+
+        # Convert slider 0-100 to data range
+        x_range_min = x_min + (x_max-x_min)*self.x_min_slider.value()/100
+        x_range_max = x_min + (x_max-x_min)*self.x_max_slider.value()/100
+        y_range_min = y_min + (y_max-y_min)*self.y_min_slider.value()/100
+        y_range_max = y_min + (y_max-y_min)*self.y_max_slider.value()/100
+
         self.ax.clear()
         colors = assign_colors(self.plugin)
         self.scatter = self.ax.scatter(self.plugin.df[x], self.plugin.df[y], c=colors)
@@ -64,12 +98,17 @@ class ScatterTab:
         # colors
         self.fc = self.scatter.get_facecolors()
 
+        # axes
         self.ax.set_xlabel(x)  # Axis labels
         self.ax.set_ylabel(y)
-        # self.ax.set_title(f"{len(self.plugin.selected_indices)} / {self.plugin.df.shape[0]}")
+        self.ax.set_title(f"{len(self.plugin.selected_indices)} / {self.plugin.df.shape[0]}")
+        self.ax.set_xlim(x_range_min - x_range_min*0.02, x_range_max + x_range_max*0.02)
+        self.ax.set_ylim(y_range_min - y_range_min*0.02, y_range_max + y_range_max*0.02)
+
         self.canvas.draw()
         if self.lasso: self.lasso.disconnect_events()
         self.lasso = LassoSelector(self.ax, onselect=self.on_lasso_select)
+
 
     def on_lasso_select(self, verts):
         #TODO: color highlighting of selected data points
