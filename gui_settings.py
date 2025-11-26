0b25727d963094217cdb45e0da964600a9dc3edf
Nicolas <nicolas.goldbach@epfl.ch>
2025-11-26 08:18:35 +0100

    added multi-model support

--- gui_settings.py
+++ gui_settings.py
@@ -1,11 +1,9 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QGroupBox, QLineEdit, QComboBox, QCheckBox, QFormLayout, QPushButton, QFileDialog
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QGroupBox, QLineEdit, QComboBox, QCheckBox, QFormLayout, QPushButton, QFileDialog, QHBoxLayout, QLabel
 import numpy as np
 import pandas as pd
 from .utils import status_msg
 from pymol import cmd
 
-# TODO: add support for loading multiple pdb files from csv file 
-#       with support for grid mode display in PyMOL
 
 class SettingTab:
     def __init__(self, plugin):
@@ -22,23 +20,69 @@ class SettingTab:
             placeholderText='CSV file',
             readOnly=True
         )
+        form.addRow("Load CSV:", self.load_btn)
+        form.addRow("CSV File:", self.csv_file_edit)
+        data_box.setLayout(form)
+
+        models_box = QGroupBox("Models")
+        form = QFormLayout()
+
+        # ToolTip messages
+        path_tooltip_message = "Replace sections of the path. This can be useful when storing\nmodels on a remote and mounting the remote file system to local.\nE.g. replace /home/user/designs with /mnt/cluster/designs"
+        combo_tooltip_message = "Column name in the csv specifying the path of the structures.\nPaths in this column will be used to load the models."
+        model_chkbox_message = "Whether or not to load and display this model in PyMOL"
+        suffix_message = "Suffix to add to model name to avoid duplicates"
+
+        # Model 1
         self.path_combo = QComboBox()
-        self.path_combo.setToolTip("Column name in the csv specifying the path of the structures.\nPaths in this column will be used to load the models.")
+        self.path_combo.setToolTip(combo_tooltip_message)
+
+        # Model 2
+        self.path_2_combo = QComboBox()
+        self.path_2_combo.setToolTip(combo_tooltip_message)
+        self.load_model_2_chkbox = QCheckBox()
+        self.load_model_2_chkbox.setToolTip(model_chkbox_message)
+        self.model_2_suffix = QLineEdit()
+        self.model_2_suffix.setToolTip(suffix_message)
+
+        # Model 3
+        self.path_3_combo = QComboBox()
+        self.path_3_combo.setToolTip(combo_tooltip_message)
+        self.load_model_3_chkbox = QCheckBox()
+        self.load_model_3_chkbox.setToolTip(model_chkbox_message)
+        self.model_3_suffix = QLineEdit()
+        self.model_3_suffix.setToolTip(suffix_message)
 
         # replacing paths on load
         self.path_replace = QLineEdit(placeholderText='/remote/data', clearButtonEnabled=True)
         self.path_with = QLineEdit(placeholderText='/mount/data', clearButtonEnabled=True)
         self.path_replace.textChanged.connect(self.set_replace_text)
         self.path_with.textChanged.connect(self.set_replace_text)
-        self.path_replace.setToolTip("Replace sections of the path. This can be useful when storing\nmodels on a remote and mounting the remote file system to local.\nE.g. replace /home/user/designs with /mnt/cluster/designs")
+        self.path_replace.setToolTip(path_tooltip_message)
 
-        form.addRow("Load CSV:", self.load_btn)
-        form.addRow("CSV File:", self.csv_file_edit)
-        form.addRow("Path:", self.path_combo)
-        form.addRow("Path Replace:", self.path_replace)
-        form.addRow("with:", self.path_with)
+        # Layout
+        replacement_box = QHBoxLayout()
+        for widget in [QLabel("Replace"), self.path_replace, QLabel("with"), self.path_with]:
+            replacement_box.addWidget(widget)
 
-        data_box.setLayout(form)
+        model2_hbox = QHBoxLayout()
+        model2_hbox.addWidget(self.path_2_combo)
+        model2_hbox.addWidget(self.load_model_2_chkbox)
+        model2_hbox.addWidget(QLabel("        suffix:"))
+        model2_hbox.addWidget(self.model_2_suffix)
+
+        model3_hbox = QHBoxLayout()
+        model3_hbox.addWidget(self.path_3_combo)
+        model3_hbox.addWidget(self.load_model_3_chkbox)
+        model3_hbox.addWidget(QLabel("        suffix:"))
+        model3_hbox.addWidget(self.model_3_suffix)
+
+        form.addRow("Model 1:", self.path_combo)
+        form.addRow("Model 2:", model2_hbox)
+        form.addRow("Model 3:", model3_hbox)
+        form.addRow(replacement_box)
+        models_box.setLayout(form)
+        
 
         # Reference Structure Box
         reference_box = QGroupBox("Reference Structure")
@@ -57,11 +101,14 @@ class SettingTab:
         color_names = [name for name, index in color_tuples]
         self.ref_color_combo.addItems(sorted(color_names))
         self.ref_color_combo.setCurrentIndex(self.ref_color_combo.findText('gray'))
+        self.ref_in_all_chckbox = QCheckBox()
+        self.ref_in_all_chckbox.setToolTip("Will display reference structure in all slots if enabling grid mode")
 
         form.addRow("Load Reference:", self.ref_btn)
         form.addRow("Reference File:", self.ref_file_edit)
         form.addRow("Align models to reference:", self.align_ref)
         form.addRow("Color reference:", self.ref_color_combo)
+        form.addRow("Display in all slots:", self.ref_in_all_chckbox)
 
         reference_box.setLayout(form)
 
@@ -73,10 +120,19 @@ class SettingTab:
             clearButtonEnabled=True
         )
         self.command_edit.setToolTip("This command will be executed when loading structures.\nPyMOL syntax e.g. color skyblue; show sticks; util.cnc")
+        self.grid_mode_chkbox = QCheckBox()
+        self.grid_mode_chkbox.setToolTip("Enable grid mode if multiple models are loaded")
+        self.group_models_chkbox = QCheckBox()
+        self.group_models_chkbox.setChecked(True)
+        self.group_models_chkbox.setToolTip("Create groups with the different models")
         form.addRow("Load Command:", self.command_edit)
+        form.addRow("Use grid mode:", self.grid_mode_chkbox)
+        form.addRow("Group models:", self.group_models_chkbox)
         appearance_box.setLayout(form)
 
+        ## Global Layout
         layout.addWidget(data_box)
+        layout.addWidget(models_box)
         layout.addWidget(reference_box)
         layout.addWidget(appearance_box)
 
@@ -92,6 +148,10 @@ class SettingTab:
         self.plugin.not_numeric_cols = [c for c in df.columns if c not in self.plugin.numeric_cols]
         self.path_combo.clear()
         self.path_combo.addItems(self.plugin.not_numeric_cols)
+        self.path_2_combo.clear()
+        self.path_2_combo.addItems(self.plugin.not_numeric_cols)
+        self.path_3_combo.clear()
+        self.path_3_combo.addItems(self.plugin.not_numeric_cols)
         self.csv_file_edit.setText(str(csv_path))
 
         # updating scatter tab
--- pymol_sync.py
+++ pymol_sync.py
@@ -1,3 +1,4 @@
+import pymol
 from pymol import cmd
 from .utils import status_msg
 import os
@@ -5,6 +6,12 @@ import numpy as np
 
 
 def sync_with_pymol(plugin, selected_indices, exclude_classified, use_og_df: bool=False):
+
+    # PyMOL is weird
+    # Need to disable undo to fix broken Selectors
+    # https://github.com/schrodinger/pymol-open-source/issues/336
+    cmd.undo_disable()
+    cmd.delete("all")
 
     if use_og_df: df = plugin.og_df
     else: df = plugin.df
@@ -31,34 +38,102 @@ def sync_with_pymol(plugin, selected_indices, exclude_classified, use_og_df: boo
     if len(selected) > max_models:
         selected = selected.sample(max_models)  # Random selection
 
-    # loading selected models
-    cmd.delete("all")
-    loaded = 0
-    for _, row in selected.iterrows():
-        p = row[plugin.setting_tab_obj.path_combo.currentText()]
-        if plugin.path_replace != None:
-            p = p.replace(plugin.path_replace[0], plugin.path_replace[1])
-        if os.path.exists(p):
-            cmd.load(p)
-            loaded += 1
-        else:
-            status_msg(f"file not found: {p}", color="red")
+    # managing grid modes
+    columns = [plugin.setting_tab_obj.path_combo.currentText()]
+    suffix = [""]
 
-    # Execute on-load command
-    if plugin.setting_tab_obj.command_edit.text() != "":
-        cmd.do(plugin.setting_tab_obj.command_edit.text())
+    if plugin.setting_tab_obj.load_model_2_chkbox.isChecked():
+        columns.append(plugin.setting_tab_obj.path_2_combo.currentText())
+        suffix.append(plugin.setting_tab_obj.model_2_suffix.text())
+
+    if plugin.setting_tab_obj.load_model_3_chkbox.isChecked():
+        columns.append(plugin.setting_tab_obj.path_3_combo.currentText())
+        suffix.append(plugin.setting_tab_obj.model_3_suffix.text())
+
+    # grid mode setup
+    if plugin.setting_tab_obj.grid_mode_chkbox.isChecked():
+        cmd.set("grid_mode", 1)
+        cmd.set("grid_max", len(columns))
+    else:
+        cmd.set("grid_mode", 0)
 
     # Load reference structure
+    ref_obj_name = None
     if plugin.reference_structure != None:
         cmd.load(plugin.reference_structure)
         ref_obj_name = os.path.basename(plugin.reference_structure).split(".")[0]
         cmd.color(plugin.setting_tab_obj.ref_color_combo.currentText(), ref_obj_name)
 
-        # Align ref structure
-        if plugin.setting_tab_obj.align_ref.isChecked():
-            active_objects = cmd.get_object_list("enabled")
-            for obj in active_objects:
-                if obj != ref_obj_name:
-                    cmd.align(obj, ref_obj_name)
+        # Display reference in first grid slot by default
+        if plugin.setting_tab_obj.grid_mode_chkbox.isChecked():
+            cmd.set("grid_slot", 1, ref_obj_name)
+        if plugin.setting_tab_obj.grid_mode_chkbox.isChecked() and plugin.setting_tab_obj.ref_in_all_chckbox.isChecked():
+            cmd.set("grid_slot", -2, ref_obj_name)
+            print(f"display {ref_obj_name} in all slots")
+
+    # loading selected models
+    loaded = 0
+    groups = {}
+    for _, row in selected.iterrows():
+        
+        paths = row[columns].values
+        
+        if plugin.path_replace != None:
+            paths = [p.replace(plugin.path_replace[0], plugin.path_replace[1]) for p in paths]
+        
+        # bug fix: will create too many grid slots
+        for i, p in enumerate(paths):
+
+            if os.path.exists(p):
+                object_name = os.path.basename(p).split(".")[0]
+                
+                if suffix[i]:
+                    object_name = object_name + "_" + suffix[i]
+
+                cmd.load(p, object=object_name)
+                loaded += 1
+
+                # align model to reference
+                if plugin.setting_tab_obj.align_ref.isChecked() and ref_obj_name != None:
+                    try:
+                        cmd.refresh()
+                        status_msg(f"aligning {object_name} to {ref_obj_name}", color="yellow")
+                        print(cmd.get_names("objects"))
+                        cmd.align(object_name, ref_obj_name)
+                    except pymol.CmdException as err:
+                        status_msg(f"failed to align model {object_name} to {ref_obj_name}", color="red")
+                
+                # Set grid slots
+                if plugin.setting_tab_obj.grid_mode_chkbox.isChecked():
+                    cmd.set("grid_slot", i+1, object_name)
+
+                # grouping
+                if plugin.setting_tab_obj.group_models_chkbox.isChecked() and len(paths) > 1:
+                    if i == 0:
+                        # init group here
+                        groupname = object_name + "_group"
+                        groups[groupname] = [object_name]
+                        # cmd.group(groupname, object_name)
+                    else:
+                        # add other models here
+                        groups[groupname].append(object_name)
+                        # cmd.group(groupname, object_name)
+
+            else:
+                status_msg(f"file not found: {p}", color="red")
+
+    # Execute on-load command
+    if plugin.setting_tab_obj.command_edit.text() != "":
+        cmd.do(plugin.setting_tab_obj.command_edit.text())
+
+    # color reference object
+    if ref_obj_name != None:
+        cmd.color(plugin.setting_tab_obj.ref_color_combo.currentText(), ref_obj_name)
     
+    # create groups - seems more robust after alignment
+    if plugin.setting_tab_obj.group_models_chkbox.isChecked() and len(paths) > 1:
+        for group in groups:
+            cmd.group(group, " ".join(groups[group]))
+    
+    cmd.center("all", animate=0)
     status_msg(f"Loaded {loaded} models into PyMOL")
